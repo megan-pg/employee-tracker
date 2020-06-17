3623979b352a6131d3e325f46404d2dd3fd59541
megan-pg <megan.pardygokcu@unh.edu>
2020-06-17 18:01:17 -0400

    made changes

--- employee-db.sql
+++ employee-db.sql
@@ -3,45 +3,141 @@ CREATE database employee_db;
 
 USE employee_db;
 
-CREATE TABLE department (
-  id INT NOT NULL AUTO_INCREMENT,
-  dept_name VARCHAR(30) NOT NULL,
-  PRIMARY KEY (id)
+CREATE TABLE department
+(
+  id INT NOT NULL
+  AUTO_INCREMENT,
+  dept_name VARCHAR
+  (30) NOT NULL,
+  PRIMARY KEY
+  (id)
 );
 
-CREATE TABLE role (
-  id INT NOT NULL AUTO_INCREMENT,
-  title VARCHAR(30) NOT NULL,
-  salary DECIMAL(9,2) NOT NULL,
+  CREATE TABLE role
+  (
+    id INT NOT NULL
+    AUTO_INCREMENT,
+  title VARCHAR
+    (30) NOT NULL,
+  salary DECIMAL
+    (9,2) NOT NULL,
   department_id INT,
-  PRIMARY KEY (id),
-  FOREIGN KEY (department_id) 
-  	REFERENCES department(id)
-    ON UPDATE CASCADE ON DELETE SET NULL
-);
+  PRIMARY KEY
+    (id),
+  FOREIGN KEY
+    (department_id) 
+  	REFERENCES department
+    (id)
+    ON
+    UPDATE CASCADE ON DELETE SET NULL
+    );
 
-CREATE TABLE employee (
-  emp_id INT NOT NULL AUTO_INCREMENT,
-  first_name VARCHAR(30) NOT NULL,
-  last_name VARCHAR(30) NOT NULL,
+    CREATE TABLE employee
+    (
+      emp_id INT NOT NULL
+      AUTO_INCREMENT,
+  first_name VARCHAR
+      (30) NOT NULL,
+  last_name VARCHAR
+      (30) NOT NULL,
   role_id INT,
   manager_id INT,
-  PRIMARY KEY (emp_id),
-  FOREIGN KEY (role_id) 
-  	REFERENCES role(id)
-    ON UPDATE CASCADE ON DELETE SET NULL,
-  FOREIGN KEY (manager_id) 
-  	REFERENCES employee(emp_id)
+  PRIMARY KEY
+      (emp_id),
+  FOREIGN KEY
+      (role_id) 
+  	REFERENCES role
+      (id)
+    ON
+      UPDATE CASCADE ON DELETE SET NULL,
+  FOREIGN KEY
+      (manager_id) 
+  	REFERENCES employee
+      (emp_id)
 );
-INSERT INTO department (dept_name)
-VALUES ("Executive");
+      INSERT INTO department
+        (dept_name)
+      VALUES
+        ("CEO");
+      INSERT INTO department
+        (dept_name)
+      VALUES
+        ("Executive");
+      INSERT INTO department
+        (dept_name)
+      VALUES
+        ("Manager");
+      INSERT INTO department
+        (dept_name)
+      VALUES
+        ("Support");
 
-INSERT INTO role (title, salary, department_id)
-VALUES ("Boss", "250000", "1");
+      INSERT INTO role
+        (title, salary, department_id)
+      VALUES
+        ("CEO", "3000000", "1");
+      INSERT INTO role
+        (title, salary, department_id)
+      VALUES
+        ("Executive", "250000", "2");
+      INSERT INTO role
+        (title, salary, department_id)
+      VALUES
+        ("Manager", "75000", "3");
+      INSERT INTO role
+        (title, salary, department_id)
+      VALUES
+        ("Support Specialist", "45000", "4");
+      INSERT INTO role
+        (title, salary, department_id)
+      VALUES
+        ("Support Opperations", "45000", "4");
 
-INSERT INTO songs (title, artist, genre)
-VALUES ("strawberry", 3.25, 75);
+      INSERT INTO role
+        (title, salary, department_id)
+      VALUES
+        ("Receptionist", "35000", "5");
 
-SELECT * FROM department;
-SELECT * FROM role;
-SELECT * FROM employee;
+      INSERT INTO employee
+        (first_name, last_name, role_id, manager_id)
+      VALUES
+        ('Megan', 'Pardy', '1', NULL);
+      INSERT INTO employee
+        (first_name, last_name, role_id, manager_id)
+      VALUES
+        ('Bob', 'Dole', '2', '1');
+      INSERT INTO employee
+        (first_name, last_name, role_id, manager_id)
+      VALUES
+        ('Diana', 'Spencer', '2', '1');
+      INSERT INTO employee
+        (first_name, last_name, role_id, manager_id)
+      VALUES
+        ('Bart', 'Simpson', '3', '2');
+      INSERT INTO employee
+        (first_name, last_name, role_id, manager_id)
+      VALUES
+        ('Kate', 'Winslet', '3', '2');
+      INSERT INTO employee
+        (first_name, last_name, role_id, manager_id)
+      VALUES
+        ('June', 'Clever', '4', '3');
+      INSERT INTO employee
+        (first_name, last_name, role_id, manager_id)
+      VALUES
+        ('George', 'Costanza', '4', '3');
+      INSERT INTO employee
+        (first_name, last_name, role_id, manager_id)
+      VALUES
+        ('Gene', 'Wilder', '4', '3');
+      INSERT INTO employee
+        (first_name, last_name, role_id, manager_id)
+      VALUES
+        ('Mel', 'Brooks', '4', '3');
+
+      SELECT *
+      FROM department;
+      SELECT *
+      FROM role;
+      SELECT *
+      FROM employee;
--- server.js
+++ server.js
@@ -1,5 +1,9 @@
 const mysql = require("mysql");
-require('dotenv').config();
+const inquirer = require("inquirer");
+const dotenv = require('dotenv').config();
+const db = require("./db");
+
+const table = require("console.table");
 
 const connection = mysql.createConnection({
     host: "localhost",
@@ -12,11 +16,77 @@ const connection = mysql.createConnection({
 
     // Your password
     password: "Happy1786!!",
-    database: "playlist_db"
+    database: "employee-db"
 });
 
-connection.connect(function (err) {
-    if (err) throw err;
-    console.log("connected as id " + connection.threadId);
-    connection.end();
-});
+const start = () => {
+
+
+
+    connection.connect(function start() {
+
+        inquirer.prompt({
+            name: "action",
+            type: "list",
+            message: "What would you like to do?",
+            choices: [
+                "View all employees",
+                "View all roles",
+                "View all departments",
+                "Add employee",
+                "Add role",
+                "Add department",
+                "Remove employee",
+                "Remove department",
+                "Update employee role",
+                "Exit"
+
+            ]
+        }).then(answer => {
+
+            switch (answer.action) {
+                case "View all employees":
+                    viewAllEmployees();
+                    break;
+                case "View all roles":
+                    viewAllEmployees();
+                    break;
+                case "View all departments":
+                    viewAllEmployees();
+                    break;
+                case "Add employee":
+                    viewAllEmployees();
+                    break;
+                case "VAdd role":
+                    viewAllEmployees();
+                    break;
+                case "View all roles":
+                    viewAllEmployees();
+                    break;
+                case "Add departmen":
+                    viewAllEmployees();
+                    break;
+                case "Remove employee":
+                    viewAllEmployees();
+                    break;
+                case "Update employee role":
+                    viewAllEmployees();
+                    break;
+                case "Exit":
+                    process.exit(-1)
+                    return;
+
+
+            }
+        })
+
+    })
+
+}
+
+async function viewAllEmployees() {
+
+    const employees = await db.findAll("employee");
+    console.table(`${employees.length} employees found: `, employees);
+    start();
+}
